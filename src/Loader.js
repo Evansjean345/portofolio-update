11cefb7a457d65dc5d65f541f74214dd42f377ff
Evansjean345 <Evansjean345@gmail.com>
2023-06-12 11:44:24 +0000

    bold

--- src/Loader.js
+++ src/Loader.js
@@ -4,7 +4,7 @@ export default function Loader() {
   return (
     <div className="h-[100vh] bg-black w-full flex flex-col justify-center items-center">
         <img src="/me.jpg" alt="" className="h-40 w-40 animate-bounce rounded-full object-bottom"  />
-        <h1 className="text-white animate-bounce mt-12 text-3xl">
+        <h1 className="text-white animate-bounce mt-12 font-bold text-3xl">
           Loading ....
         </h1>
     </div>
